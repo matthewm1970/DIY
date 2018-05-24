ded712697924da985d207c53fa8aba6f958ccdd1
matthewm1970 <matthewm1970@gmail.com>
2018-05-24 13:26:05 +0200

    Update US.cpp

--- libraries/US-3pin/US.cpp
+++ libraries/US-3pin/US.cpp
@@ -10,29 +10,32 @@ US::US(int pinTrigger, int pinEcho){
 
 void US::init(int pinTrigger, int pinEcho)
 {
-  //** The 3-pin version of the ultrasonic sensor uses the same
-       pin for signaling (trigger) and reading data (echo). We
-       accept a two-parameter call for easy compatibility with
-       the 4-pin version, but we ignore the echo pin. 
-  **//
+  // The 3-pin version of the ultrasonic sensor uses the same
+  // pin for signaling (trigger) and reading data (echo). We
+  // accept a two-parameter call for easy compatibility with
+  // the 4-pin version, but we ignore the echo pin. 
   _pinTrigger = pinTrigger;
-  _pinEcho = pinTrigger;
-  pinMode( _pinTrigger , OUTPUT );
-  pinMode( _pinEcho , INPUT );
 }
 
 long US::TP_init()
 {
+    // First, trigger the US with a LOW pulse, to clear any signal out
+    // then a HIGH signal longer than 2 microseconds.
+    pinMode(pinTrigger, OUTPUT);
     digitalWrite(_pinTrigger, LOW);
     delayMicroseconds(2);
     digitalWrite(_pinTrigger, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(5);
     digitalWrite(_pinTrigger, LOW);
-    long microseconds = pulseIn(_pinEcho,HIGH,40000); //40000
-    return microseconds;
+    // Next, read the pin for input, waiting a maximum of .03 seconds for
+    // a pulse to start (for return of responsiveness, if nothing happens).
+    pinMode(_pinTrigger, INPUT);
+    long microsecondsDuration = pulseIn(_pinTrigger,HIGH,30000);
+    return microsecondsDuration;
 }
 
 float US::read(){
+  // Read the 
   long microseconds = US::TP_init();
   long distance;
   distance = microseconds/29/2;
